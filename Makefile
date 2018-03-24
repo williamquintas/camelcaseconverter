2786582efdcb4f756495a3704c3c5e4212be0b33
William Quintas de Melo <william.quintas@gmail.com>
2018-03-23 21:56:39 -0300

    tratando casos com underline

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Macros para compilacao
 CC = gcc
-CFLAGS = -Wextra
+CFLAGS = -Wextra -L/usr/local/opt/flex/lib -I/usr/local/opt/flex/include
 DIR = src
 FILENAME = $(DIR)/main.c
 OBJNAME = $(DIR)/main.o
--- src/ccc_lib.c
+++ src/ccc_lib.c
@@ -1,4 +1,4 @@
-
+#include "string.h"
 #include "ccc_lib.h"
 
 void camelcase_to_underline(char *input, char *output) {
@@ -7,10 +7,34 @@ void camelcase_to_underline(char *input, char *output) {
 }
 
 void underline_to_camelcase(char *input, char *output) {
+  //Declaração de variáveis
+  char * pch;
+  int writer = 0, reader = 0;
+  //Copia input para output
+  strcpy(output, input);
+  //Camelcase o primeiro caracter da palavra
+  output[0] = input[0] - 32;
+  //Pega a primeira posição do primeiro caracter em input
+  pch=strchr(input,'_');
+  //Enquanto não chegar no fim da palavra
+  while (pch!=NULL)
+  {
+    //Converte o caracter após o _
+    output[pch-input+1] = input[pch-input+1]-32;
+    //Procura o próximo _
+    pch=strchr(pch+1,'_');
+  }
 
+  /* TIRA _ DA SAÍDA */
+  while (output[reader])//Enquanto não chega no fim da palavra
+  {
+      if (output[reader]!='_')//Se diferente de _, copia o caracter
+      {
+          output[writer++] = output[reader];
+      }
+      reader++;
+  }
+
+  output[writer]=0;//Fecha string
   return;
 }
-
-
-
-
